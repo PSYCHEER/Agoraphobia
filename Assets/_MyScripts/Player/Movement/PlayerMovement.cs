92dcb3b72367b44a2ff59568bfeb65c5535e1c7d
PSYCHEER <42911029+PSYCHEER@users.noreply.github.com>
2024-06-22 17:56:43 +0200

    Some updates of collisions

--- Assets/_MyScripts/Player/Movement/PlayerMovement.cs
+++ Assets/_MyScripts/Player/Movement/PlayerMovement.cs
@@ -87,10 +87,13 @@ public class PlayerMovement : MonoBehaviour
 			moveVector = playerControl.Keyboard.Movement.ReadValue<Vector2>();
 			move = new Vector3(moveVector.x * moveSpeed, 0, moveVector.y * moveSpeed);
 			move.y = playerControl.Keyboard.Jump.ReadValue<float>() * jumpHeight;
+			rb.velocity = new Vector3(0,0,0);
 			//Debug.Log("X: " + moveVector.x + "Y: " + moveVector.y);
 		}
 		else
+		{
 			move.y -= gravity * Time.deltaTime;
+		}
 		transform.Translate(move * Time.deltaTime);
 	}
 	
